fa1dc075a2b34a354ad3a9668385595a88dd8153
hackTivist <bildadsimiyu6@gmail.com>
2025-06-05 15:08:17 +0300

    feat: implement user registration logic in AuthService and add AuthController

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
+import { hashPassword } from './utils/passwords';
 
 @Injectable()
 export class AuthService {
     constructor(private readonly prisma: PrismaService) {}
 
     //!REGISTER
-    async register() {}
+    async register(registerDto: RegisterDto) {
+        const {username, email, password} = registerDto;
+        //check if user with the provided email exists
+        const emailExists = await this.prisma.users.findUnique({where: {email}});
+        if(emailExists) {
+            throw new ConflictException("Invalid credentials. Try again")
+        }
+
+        //hash password
+        const hashedPassword = await hashPassword(password);
+
+        const newUser = await this.prisma.users.create({
+            data: {
+                username,
+                email,
+                password: hashedPassword
+            }
+        });
+
+        return {
+            success: true,
+            message: "User created successfully",
+            newUser
+        }
+
+    }
 
     //!LOGIN
-    async login() {}
+    async login(loginDto: LoginDto) {}
 
 }
