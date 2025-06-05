158dcc3229ca7fe08687e4a5ceaf737d56926171
hackTivist <bildadsimiyu6@gmail.com>
2025-06-05 13:42:16 +0300

    feat: integrate PrismaModule into AuthModule and update AuthService to use PrismaService

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -2,10 +2,10 @@ import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from 'generated/prisma';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit{
-    async onModuleInit() {
-        this.$connect()
-        .then(() => console.log("DB Connected"))
-        .catch(err => console.log(err));
-    }
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    this.$connect()
+      .then(() => console.log('DB Connected'))
+      .catch((err) => console.log(err));
+  }
 }
