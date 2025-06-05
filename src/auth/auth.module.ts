0fefcbb455dc4726408da16c9d1bef201e4c1104
hackTivist <bildadsimiyu6@gmail.com>
2025-06-05 14:20:42 +0300

    feat: add JwtModule and JwtStrategy for JWT authentication

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,20 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   providers: [AuthService],
   controllers: [AuthController],
-  imports: [PrismaModule]
+  imports: [PrismaModule, JwtModule.registerAsync({
+    imports: [ConfigModule],
+    useFactory: async(configService: ConfigService) => ({
+      secret: configService.get<string>('JWT_SECRET'),
+      signOptions: { expiresIn: '1h' }
+    }),
+    inject: [ConfigService]
+  })
+]
 })
 export class AuthModule {}
---
+++ src/auth/utils/strategies/jwt.strategy.ts
@@ -0,0 +1,34 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    config: ConfigService,
+    private readonly prisma: PrismaService,
+  ) {
+    //get jwt secret from config
+    const jwtSecret = config.get<string>('JWT_SECRET');
+    if (!jwtSecret) {
+      throw new UnauthorizedException('Jwt secret is not provided in config');
+    }
+
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: jwtSecret,
+    });
+  }
+
+  async validate(payload: { userId: string; email: string }) {
+    const user = await this.prisma.users.findUnique({
+      where: { id: payload.userId },
+    });
+    if (!user)
+      throw new UnauthorizedException('oops... failed to validate user');
+    return user;
+  }
+}
