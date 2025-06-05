69471c9127f51638df63bb08b49dbc71cd52cee7
hackTivist <bildadsimiyu6@gmail.com>
2025-06-05 13:20:35 +0300

    feat: Add auth module and tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaController } from './prisma/prisma.controller';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [AppController, PrismaController],
   providers: [AppService],
 })
