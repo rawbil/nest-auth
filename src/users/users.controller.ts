5277fc54ce1eed9563d43ebc1d0580019b61c6fb
hackTivist <bildadsimiyu6@gmail.com>
2025-06-06 14:36:43 +0300

    feat: add createUser controller and Global pipes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
+
+    @Post('create-user') //users/create-user
+    createUser(@Body() createUserDto: CreateUserDto) {
+        return this.usersService.createUser(createUserDto);;
+    }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,20 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { Prisma } from 'generated/prisma';
+import { hashPassword } from 'src/auth/utils/passwords';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UsersService {
-    constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
-    //!Create User
-    async createUser() {
-        
+  //!Create User
+  async createUser(data: Prisma.UsersCreateInput) {
+    //check if email already exists
+    const emailExists = await this.prisma.users.findUnique({
+      where: { email: data.email },
+    });
+    if (emailExists) {
+      throw new ConflictException('Email already exists');
     }
-    
-    //!Get all users
 
-    //!Get user with id
+    //hash password
+    const hashedPassword = await hashPassword(data.password);
 
-    //!Update user with id
+    const newUser = await this.prisma.users.create({
+      data: {
+        username: data.username,
+        password: hashedPassword,
+        email: data.email,
+      },
+    });
 
-    //!Delete user
+    return {
+        success: true,
+        message: `User ${data.email} created successfully!`,
+        newUser
+    }
+  }
+
+  //!Get all users
+
+  //!Get user with id
+
+  //!Update user with id
+
+  //!Delete user
 }
