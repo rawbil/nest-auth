bffda4a72e67f1193d595572f770bd98d51bbe70
hackTivist <bildadsimiyu6@gmail.com>
2025-06-07 19:26:25 +0300

    Add @Global decorator to make prisma module available globally

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,6 +1,7 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
+@Global() //makes the prisma module available globally 
 @Module({
   providers: [PrismaService],
   exports: [PrismaService],
